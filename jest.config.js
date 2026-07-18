1ff9ca3512226186d02c102b015030c68c5f52ce
Pela647 <robelfesshaye@gmail.com>
2026-07-18 16:27:20 -0400

    upgrade packages

--- jest.config.js
+++ jest.config.js
@@ -174,8 +174,11 @@ module.exports = {
     // A map from regular expressions to paths to transformers
     // transform: undefined,
     transform: {
-        "\\.m?js$": "jest-esm-transformer"
+        "\\.m?js$": "babel-jest"
     },
+    transformIgnorePatterns: [
+        "node_modules[\\\\/](?!make-dir[\\\\/])"
+    ],
     // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
     // transformIgnorePatterns: [
     //   "\\\\node_modules\\\\",
